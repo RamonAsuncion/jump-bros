49327e4c2e187a341bba38da5c88fdc4d2e888be
Ramon Asuncion <asuncionbatista@gmail.com>
2020-10-02 23:36:45 -0400

    Code removed for C# created problem with run

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -3,45 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 
-public class Win : MonoBehaviour
-{
-    public Text txt;
-    public int playersCounter = 0;
-
-    public void Start()
-    {
-        txt = GameObject.FindObjectType<Text>();
-    }
-
-    public void update()
-    {
-        txt.text = playersFinished + "/2 Players finished";
-        if (playersFinished == 2)
-        {
-            print("Players finished");
-            SceneManager.LoadScene("Main Menu");
-        }
-
-        public void OnTriggerEnter2D(Collider2D collision)
-        {
-            print(collision);
-            if (collision.tag == "Player")
-            {
-                print("player finished");
-                playersFinished += 1;
-            }
-        }
-
-        public void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.tag == "Player")
-            {
-                print("player left");
-                playersFinished -= 1;
-            }
-        }
-    }
+public class Win : MonoBehaviour{
+    // Code will be added to win the game. 
 }
-}
-
 
