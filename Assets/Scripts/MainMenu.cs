3cb6ad1483e3d5aa9c0eb90f4a27601eda22502a
Ramon Asuncion <asuncionbatista@gmail.com>
2020-10-02 23:55:50 -0400

    Tutorial text added to main + script to load up the scene in main.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("Level01");
     }
 
+    public void TutorialButton()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
