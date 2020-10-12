48b811e30c6f03b2cb43ba2f3917b461a04c4574
Ramon Asuncion <asuncionbatista@gmail.com>
2020-10-11 22:30:37 -0400

    Updates to the sripts

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Win : MonoBehaviour{
-    // Code will be added to win the game. 
+public class Win : MonoBehaviour
+{
+    public GameObject confettifx;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Turn on confetti");
+            GameObject ob = Instantiate(confettifx);
+            Destroy(ob, 2.5f);
+        }
+    }
 }
-
