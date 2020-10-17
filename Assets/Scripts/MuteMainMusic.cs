809f406b20c8f120677150a6c7042c09abac75a7
Ramon Asuncion <asuncionbatista@gmail.com>
2020-10-16 21:33:18 -0400

    Mute button now a toggle, icon change, and repositioning

--- Assets/Scripts/MuteMainMusic.cs
+++ Assets/Scripts/MuteMainMusic.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MuteMainMusic : MonoBehaviour
 {
-    // Start is called before the first frame update
     private bool muteMusic;
+    [SerializeField] Toggle audioToggle;
 
     void Start()
     {
-        muteMusic = false;
+        muteMusic = PlayerPrefs.GetInt("MUTED") == 1;
+        AudioListener.pause = muteMusic;
     }
 
-    // Update is called once per frame
     public void pressMuteButton()
     {
         muteMusic = !muteMusic;
         AudioListener.pause = muteMusic;
+        PlayerPrefs.SetInt("MUTED", muteMusic ? 1 : 0);
     }
 }
